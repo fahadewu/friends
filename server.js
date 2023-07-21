46b97b5783b13732fa1b6cad153b621a65ced31e
fahadewu <2021-1-50-002@std.ewubd.edu>
2023-07-21 14:41:48 +0600

    Basic Server Creation

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('this is a test')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    }
+)
+
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+    }
+)
